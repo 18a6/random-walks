15973652465e7bdd3bdd859d0784c9bf7f434e92
18a6 <ar.brhl49@gmail.com>
2023-08-14 09:52:55 +0100

    feat: defined Location, Drunk and Field

--- location.py
+++ location.py
@@ -1,3 +1,5 @@
+import random
+
 class Location(object):
     def __init__(self, x, y):
         """x and y are floats"""
@@ -14,3 +16,53 @@ class Location(object):
     
     def getY(self):
         return self.y
+    
+    def distFrom(self, other):
+        xDist = self.x - other.getX()
+        yDist = self.y - other.getY()
+        return (xDist**2 + yDist**2)**0.5
+    
+    def __str__(self):
+        return '<' + str(self.x) + ', ' + str(self.y) + '>'
+
+# base class, it's only purpose is to be inherited    
+class Drunk(object):
+    def __init__(self, name = None):
+        """assumes name is a str"""
+        self.name = name
+
+    def __str__(self):
+        if self != None:
+            return self.name
+        return "anon"
+    
+class UsualDrunk(Drunk):
+    def takeStep(self):
+        stepChoices = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        return random.choice(stepChoices)
+
+class MasochistDrunk(Drunk):
+    def takeStep(self):
+        stepChoices = [(0.0, 1.1), (0.0, -0.9), (1.0, 0.0), (-1.0, 0,0)] #biased random walk
+        return random.choice(stepChoices)
+
+class Field(object):
+    def __init__(self):
+        self.drunks = {}
+
+    def addDrunk(self, drunk, loc):
+        if drunk in self.drunks:
+            raise ValueError('Duplicate drunk')
+        else:
+            self.drunks[drunk] = loc
+
+    def getDrunk(self, drunk):
+        if drunk not in self.drunks:
+            raise ValueError('Drunk not in field')
+        return self.drunks[drunk]
+
+    def moveDrunk(self, drunk):
+        if drunk not in self.drunks:
+            raise ValueError('Drunk not in field')
+        xDist, yDist = drunk.takeStep()
+        self.drunks[drunk] = self.drunks[drunk].move(xDist, yDist)
