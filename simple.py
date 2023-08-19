ded6850b7f29e5c0f3ce6f8b062e9f570a463904
18a6 <ar.brhl49@gmail.com>
2023-08-19 02:58:38 +0100

    feat: added logic for simulation

--- simple.py
+++ simple.py
@@ -1,6 +1,18 @@
+import random
+
 class walker():
     def __init__(self, x, y):
         """x and y are floats, inittial position of the walker"""
         self.x = x
         self.y = y 
-   
+
+    def walk(self):
+        self.x += random.choice(-1, 1) * random.random
+        self.y += random.choice(-1, 1) * random.random
+
+
+def simulate():
+    w = walker(0, 0)
+    w.walk()
+    
+simulate()
